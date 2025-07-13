0ad08ca85433ed8fba0ea8dd0db43bad997982f7
muzheZ <mzeng6@wisc.edu>
2025-07-13 11:22:51 -0400

    fill in config files

---
+++ ml_pipeline_tutorial/ml_pipeline/config.py
@@ -0,0 +1,47 @@
+"""Pipeline configuration.
+
+This module loads configuration information from files in the config directory.
+"""
+
+import pathlib
+
+from omegaconf import OmegaConf
+
+
+class Config:
+    """Pipeline configuration."""
+
+    def __init__(self, config_dir: str, project: str) -> None:
+        """Instantiates the config object.
+
+        Args:
+            config_dir (str): Path to config directory.
+            project (str): Project name. Must correspond to a YAML file.
+        """
+        self.config_dir = pathlib.Path(config_dir)
+        self.project = project
+
+    def load(self) -> None:
+        """Loads configuration into the config object.
+
+        Raises:
+            FileNotFoundError: File not found.
+            PermissionError: Insufficient permissions.
+            IsADirectoryError: Project config path points to a directory.
+            yaml.scanner.ScannerError: Invalid YAML.
+        """
+        self.items = OmegaConf.merge(
+            OmegaConf.load(self.config_dir / "common.yaml"),
+            OmegaConf.load(self.config_dir / "datasets.yaml"),
+            OmegaConf.load(self.config_dir / f"projects/{self.project}.yaml"),
+        )
+
+    def __repr__(self):
+        """Printable representation of an object of this type."""
+        return OmegaConf.to_yaml(self.items)
+
+
+if __name__ == "__main__":
+    config = Config("config", "iris_classification")
+    config.load()
+    print(config)
